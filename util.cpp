43827a8a1f18ae9f8c42c652492ee3b620778e51
jiewaikexue <2947471438@qq.com>
2023-03-20 10:01:14 +0800

    超时模块

--- util.cpp
+++ util.cpp
@@ -1,6 +1,11 @@
 
 #include "util.h"
 
+/*
+    关于超时任务:
+        1. 如何判断是否超时: 时间戳 < 当前时间戳 ? 超时 放弃调度 : 不超时 可以调度
+        2. wakeup时 : 是在 不超时的前提下 才能进行调度: 一旦未超时的 fiber 被唤醒, 超时时间作废
+*/
 namespace util {
 
 //获取一个相对时间
@@ -10,7 +15,7 @@ int64_t NowMs() {
     //函数原型:int gettimeofday(struct timeval*tv, struct timezone *tz);
     gettimeofday(&tv, nullptr);
     // 秒数*1000 + 微妙/1000
-    // int64_t 防止移除 32 最多到2038年
+    // int64_t 防止溢出 32 最多到2038年
     return int64_t(tv.tv_sec * 1000) + tv.tv_usec / 1000;
 }
 
